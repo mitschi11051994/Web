7ab9f8c8078f3cb30140392b5260ef913e3b209c
mitschi11051994 <mitschi1994@hotmail.com>
2017-08-21 09:52:59 -0600

    GET ALL CLIENTS

--- Web/Controllers/API/tblClientController.cs
+++ Web/Controllers/API/tblClientController.cs
@@ -4,10 +4,32 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Web.Models;
 
 namespace Web.Controllers.API
 {
     public class tblClientController : ApiController
     {
+        public IHttpActionResult GetAlltblClient()
+        {
+            using (var ctx = new CMDEntities())
+            {
+                clientes = ctx.tblClient.Select(s => new tblClientViewModel()
+                {
+                    id_client = s.id_client,
+                    name = s.name,
+                    web_page = s.web_page,
+                    direccion = s.direccion,
+                    tel = s.tel,
+                    puesto= s.puesto}).ToList<tblClientViewModel>();
+            }
+
+            if (clientes.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(clientes);
+        }
     }
 }
