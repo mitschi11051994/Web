569aa17d7a0c9c4610fdef5c9ca45bcb25a6ae59
mitschi11051994 <mitschi1994@hotmail.com>
2017-08-21 10:13:09 -0600

    Add Controller Meth. Gets

--- Web/Controllers/API/tblClientController.cs
+++ Web/Controllers/API/tblClientController.cs
@@ -32,6 +32,61 @@ namespace Web.Controllers.API
             return Ok(clientes);
         }
 
+        public IHttpActionResult GettblClientById(int id)
+        {
+            tblClientViewModel clientes = null;
+
+            using (var ctx = new CMDEntities())
+            {
+                clientes = ctx.tblClient.Where(s => s.id_client == id)
+                    .Select(s => new tblClientViewModel()
+                    {
+                        id_client = s.id_client,
+                        name = s.name,
+                        web_page = s.web_page,
+                        direccion = s.direccion,
+                        tel = s.tel,
+                        puesto = s.puesto
+                    }).FirstOrDefault<tblClientViewModel>();
+            }
+
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(clientes);
+        }
+
+        public IHttpActionResult GetAllStblClient(string name)
+        {
+            IList<tblClientViewModel> clientes = null;
+
+            using (var ctx = new CMDEntities())
+            {
+                clientes = ctx.tblClient.Where(s => s.name.ToLower() == name.ToLower())
+                    .Select(s => new tblClientViewModel()
+                    {
+                        id_client = s.id_client,
+                        name = s.name,
+                        web_page = s.web_page,
+                        direccion = s.direccion,
+                        tel = s.tel,
+                        puesto = s.puesto
+                    }).ToList<tblClientViewModel>();
+            }
+
+
+            if (clientes.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(clientes);
+
+        }
+
+
 
 
     }
