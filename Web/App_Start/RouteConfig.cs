415d6eb6c0727525aedbefd1b4c9b646d9526cca
mitschi11051994 <mitschi1994@hotmail.com>
2017-08-21 11:05:27 -0600

    Edit View tblClient

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -18,6 +18,69 @@ namespace Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            
+
+
+
+            routes.MapRoute(
+                name: "Entrar Usuario",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblLogin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Crear_Usuario",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblLogin/Index", action = "Create", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Cliente",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblClient", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "crear Cliente",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblClient/Index/", action = "Create", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "tblContacts",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblContact", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "crear tblContacts",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblContact/Index/", action = "Create", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "tblReunions1",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblReunion", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "crear tblReunions1",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblReunion/Index/", action = "Create", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "tblSupport_Tickets",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "tblSupport_Ticket", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+                name: "crear tblSupport_Tickets",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "tblSupport_Ticket/Index/", action = "Create", id = UrlParameter.Optional }
+            );
         }
     }
 }
--- Web/Controllers/tblClientController.cs
+++ Web/Controllers/tblClientController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -11,7 +13,33 @@ namespace Web.Controllers
         // GET: tblClient
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<tblClientViewModel> clientes = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:4701/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("tblClient");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<tblClientViewModel>>();
+                    readTask.Wait();
+
+                    clientes = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    clientes = Enumerable.Empty<tblClientViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(clientes);
         }
     }
 }
